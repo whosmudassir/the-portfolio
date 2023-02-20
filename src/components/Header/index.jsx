d79f9521820c496ea1d696c2c14c96a1d110bdb1
whosmudassir <deadsoulartist@gmail.com>
2023-02-20 21:01:14 +0530

    feat: header ui

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <nav class="bg-white bg-opacity-75 backdrop-blur-md fixed top-0 left-0 right-0 z-50">
       <div class="container mx-auto px-4">
-        <div class="flex justify-between items-center py-4">
+        <div class="flex justify-between items-center py-2 ">
           <div class="flex items-center">
-            <img class="h-8 w-auto mr-2" src="/path/to/logo.png" alt="Logo" />
+            {/* <Image
+              src={logo}
+              alt="Picture of the author"
+              width={55}
+              height={-50}
+            /> */}
           </div>
-          <div class="text-center">
-            <h1 class="text-xl font-semibold text-gray-900 tracking-tight">
+          <div class="text-center flex gap-5">
+            <p class="text-xl font-semibold text-gray-900 tracking-tight">
               Brand Name
-            </h1>
+            </p>{" "}
+            <p class="text-xl font-semibold text-gray-900 tracking-tight">
+              Brand Name
+            </p>{" "}
+            <p class="text-xl font-semibold text-gray-900 tracking-tight">
+              Brand Name
+            </p>{" "}
+            <p class="text-xl font-semibold text-gray-900 tracking-tight">
+              Brand Name
+            </p>
           </div>
           <div class="flex items-center">
             <img class="h-8 w-auto mr-2" src="/path/to/logo.png" alt="Logo" />
