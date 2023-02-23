624860ec71e7ab152d2a633b214734ed7b313ce8
whosmudassir <deadsoulartist@gmail.com>
2023-02-23 20:52:44 +0530

    feat: grid layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,24 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import { motion } from "framer-motion";
 import Header from "@/components/Header";
+import Card from "@/components/Card";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <img
-        src={
-          "https://is3-ssl.mzstatic.com/image/thumb/wu6bl_EDpI5bOnLWSN0mRQ/1679x945sr.webp"
-        }
-      />
+    <div
+      className={
+        "flex-1 flex-col pt-[50px] grid grid-cols-4 gap-4 md:grid-cols-6"
+      }
+    >
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
