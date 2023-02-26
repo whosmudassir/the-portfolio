2b76c886be1c398d4e7fe19e88e55f7343909950
whosmudassir <deadsoulartist@gmail.com>
2023-02-26 10:57:18 +0530

    feat: hover, transitions on card comp

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Card = () => {
-  return <div className="h-[500px] bg-indigo-500 w-[200px] ">Card</div>;
+  return (
+    <div className="h-[300px] bg-[#f8f8f8] w-full rounded-md cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out">
+      Card
+    </div>
+  );
 };
 
 export default Card;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <nav className="bg-white bg-opacity-75 backdrop-blur-md fixed top-0 left-0 right-0 z-50">
+    <nav className="bg-white bg-opacity-75 backdrop-blur-md fixed top-0 left-0 right-0 z-50 ">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-3">
           <div className="flex items-center">
