f302bb564048be8003cdca599fb6b05d911b4e63
whosmudassir <deadsoulartist@gmail.com>
2023-02-27 16:35:24 +0530

    feat: added icons, github, insta cards

--- src/components/Cards/GithubCard/index.jsx
+++ src/components/Cards/GithubCard/index.jsx
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
+import Image from "next/image";
+import github from "../../../assets/icons/github.svg";
 
-const GithubCard = () => {
+const PhoneCard = () => {
   return (
     <div
       style={{ userSelect: "none" }}
-      className="h-[300px] bg-[#f8f8f8] w-full rounded-lg cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around p-[30px]"
+      className="h-[100px] bg-[#906cf2] w-full rounded-lg cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around p-[30px]"
       //   onClick={loopThroughImgs}
     >
       <div>
-        {/* <Image src={imgArray[browseImg]} alt="img" width={180} height={180} /> */}
-      </div>
-
-      <div>
-        {/* <p>Hey there ! I'm,</p>
-        <p>Mudassir Khan</p> */}
+        <Image src={github} alt="img" width={65} height={65} />
       </div>
     </div>
   );
 };
 
-export default GithubCard;
+export default PhoneCard;
