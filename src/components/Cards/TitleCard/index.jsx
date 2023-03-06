4af2a87d75580f5eea2486f319b8ae1c520cdf42
whosmudassir <deadsoulartist@gmail.com>
2023-03-06 20:42:32 +0530

    feat: title card

---
+++ src/components/Cards/TitleCard/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+import doc from "../../../assets/icons/doc.svg";
+
+const TitleCard = () => {
+  return (
+    <div
+      style={{ userSelect: "none" }}
+      className="h-full bg-[#f8f8f8] w-full rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around p-[30px]"
+      //   onClick={loopThroughImgs}
+    >
+      <div>
+        <p>FrontEnd Developer</p>
+      </div>
+    </div>
+  );
+};
+
+export default TitleCard;
