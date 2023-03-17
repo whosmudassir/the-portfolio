cd05a5a8dc8af209014a6b8d59ab1604f2e8d3d4
whosmudassir <deadsoulartist@gmail.com>
2023-03-17 18:17:58 +0530

    feat: chatter app banner

--- src/components/Cards/WorkCard/index.jsx
+++ src/components/Cards/WorkCard/index.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import Image from "next/image";
-import happy from "../../../assets/images/happy-mac.png";
 
 const WorkCard = () => {
   return (
     <div
       style={{ userSelect: "none" }}
-      className="h-[200px] bg-[#ebecff] w-full  rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around"
+      className="h-[220px] bg-[#f8f8f8] w-full  rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around p-[20px]"
       //   onClick={loopThroughImgs}
     >
-      <div>
-        <Image src={happy} alt="img" width={65} height={65} />
+      {/* <div>
+        <Image src={happy} alt="img" width={"100%"} height={"200px"} />
+      </div> */}
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <p className="chat-sent-01">Hey</p>
+        <p className="chat-sent-02">
+          Built this chat app <br />{" "}
+          <p className="link-text">https://mudassir-chatter.web.app/</p>
+        </p>
+        <p className="chat-receive">Awesome 🔥</p>
       </div>
-      <p>Work</p>
     </div>
   );
 };
