67c32d3a29e7b5e2884609310574be09fd318b5f
whosmudassir <deadsoulartist@gmail.com>
2023-03-21 18:52:04 +0530

    feat: new ui comps

---
+++ src/components/Cards/MoreProjects/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "next/image";
+
+const MoreProjects = () => {
+  return (
+    <div
+      style={{ userSelect: "none" }}
+      className="h-[220px] bg-[#f8f8f8] w-full  rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around p-[20px]"
+      //   onClick={loopThroughImgs}
+    >
+      {/* <div>
+        <Image src={happy} alt="img" width={"100%"} height={"200px"} />
+      </div> */}
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <p> More Work</p>
+      </div>
+    </div>
+  );
+};
+
+export default MoreProjects;
