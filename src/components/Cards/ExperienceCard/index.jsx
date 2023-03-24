d2d7b05e6a9020f7d926c5e69dae10bd5a1cf499
whosmudassir <deadsoulartist@gmail.com>
2023-03-24 17:59:08 +0530

    feat: overflow auto on fixed height elements

--- src/components/Cards/ExperienceCard/index.jsx
+++ src/components/Cards/ExperienceCard/index.jsx
@@ -6,7 +6,7 @@ const ExperienceCard = () => {
   return (
     <div
       style={{ userSelect: "none" }}
-      className="h-[300px] p-[30px] bg-[#ebecff] w-full  rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex  flex-col  "
+      className="h-[300px] p-[30px] bg-[#ebecff] w-full  rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex  flex-col overflow-auto "
       //   onClick={loopThroughImgs}
     >
       <div className="font-Inter font-medium text-lg self-start pb-[10px] text-[#474636]">
@@ -16,6 +16,11 @@ const ExperienceCard = () => {
       <div className="flex  flex-col items-center justify-center">
         <LanguageWithIcon icon={geekyants} text={"GeekyAnts"} />
         <div className="flex flex-col items-center justify-center text-center font-Inter font-medium text-base  pt-[8px] ">
+          <p>
+            Software Engineer - II <br />{" "}
+            <p className="text-xs">Aug 2022 - present</p>
+          </p>
+          <div className="h-8 w-0.5 bg-[#9EA3F7] mx-auto my-2"></div>
           <p>
             Software Engineer - II <br />{" "}
             <p className="text-xs">Aug 2022 - present</p>
