df1a45a680252d788d45038632c83c3270663cac
whosmudassir <deadsoulartist@gmail.com>
2023-03-24 19:28:32 +0530

    feat: adding links

--- src/components/Cards/GithubCard/index.jsx
+++ src/components/Cards/GithubCard/index.jsx
@@ -4,15 +4,17 @@ import github from "../../../assets/icons/github.svg";
 
 const PhoneCard = () => {
   return (
-    <div
-      style={{ userSelect: "none" }}
-      className="h-[100px] bg-[#ebecff] w-full  rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around"
-      //   onClick={loopThroughImgs}
-    >
-      <div>
-        <Image src={github} alt="img" width={65} height={65} />
+    <a href="https://github.com/whosmudassir" target="_blank">
+      <div
+        style={{ userSelect: "none" }}
+        className="h-[100px] bg-[#ebecff] w-full  rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around"
+        //   onClick={loopThroughImgs}
+      >
+        <div>
+          <Image src={github} alt="img" width={65} height={65} />
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
--- src/components/Cards/LinkedInCard/index.jsx
+++ src/components/Cards/LinkedInCard/index.jsx
@@ -2,18 +2,23 @@ import React, { useState } from "react";
 import Image from "next/image";
 import linkedin from "../../../assets/icons/linkedin-circle.svg";
 
-const PhoneCard = () => {
+const LinkedInCard = () => {
   return (
-    <div
-      style={{ userSelect: "none" }}
-      className="h-[100px] bg-[#9EF7C6] w-full  rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around "
-      //   onClick={loopThroughImgs}
+    <a
+      href="https://www.linkedin.com/in/mudassir-khan-08139a195"
+      target="_blank"
     >
-      <div>
-        <Image src={linkedin} alt="img" width={65} height={65} />
+      <div
+        style={{ userSelect: "none" }}
+        className="h-[100px] bg-[#9EF7C6] w-full  rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around "
+        //   onClick={loopThroughImgs}
+      >
+        <div>
+          <Image src={linkedin} alt="img" width={65} height={65} />
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
-export default PhoneCard;
+export default LinkedInCard;
