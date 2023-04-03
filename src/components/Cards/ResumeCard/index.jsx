fd79cd2d322e5dc4873a3206b994a9f0ee2f2dd2
whosmudassir <deadsoulartist@gmail.com>
2023-04-03 18:46:57 +0530

    feat: new icons added

--- src/components/Cards/ResumeCard/index.jsx
+++ src/components/Cards/ResumeCard/index.jsx
@@ -4,19 +4,23 @@ import doc from "../../../assets/icons/doc.svg";
 
 const ResumeCard = () => {
   return (
-    <div
-      style={{ userSelect: "none" }}
-      className="h-[120px] w-[full] bg-[#9ef3f7] w-full rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around p-[30px]"
-      //   onClick={loopThroughImgs}
+    <a
+      href="https://drive.google.com/file/d/1opMPFmZ0L_vfzZKe4MTwdSnou6TXzoxd/view?usp=sharing"
+      target={"_blank"}
     >
-      <div>
-        <Image src={doc} alt="img" width={65} height={65} />
-      </div>
+      <div
+        style={{ userSelect: "none" }}
+        className="h-[120px] w-[full] bg-[#9ef3f7] w-full rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex items-center justify-around p-[30px]"
+      >
+        <div>
+          <Image src={doc} alt="img" width={65} height={65} />
+        </div>
 
-      <div>
-        <p className="font-Inter font-medium text-md">View Resume</p>
+        <div>
+          <p className="font-Inter font-medium text-md">View Resume</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
