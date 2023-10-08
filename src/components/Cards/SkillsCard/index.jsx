4c10ff6804f11bbfbd8e533568485120a838ec17
deadsoulartist <deadsoulartist@gmail.com>
2023-10-08 10:15:05 +0530

    chore: css update

--- src/components/Cards/SkillsCard/index.jsx
+++ src/components/Cards/SkillsCard/index.jsx
@@ -22,7 +22,7 @@ const SkillsCard = () => {
       className="h-[578px] bg-[#ebecff] w-full  rounded-2xl cursor-pointer hover:scale-95 transform transition duration-500 ease-in-out flex  flex-col items-center justify-center p-[30px] overflow-auto "
       //   onClick={loopThroughImgs}
     >
-      <div className="font-Inter font-medium text-lg self-start pb-[10px] text-[#363947]">
+      <div className="sticky top-0 font-Inter font-medium text-lg self-start pb-[10px] text-[#363947]">
         <p>Skills</p>
       </div>
 
